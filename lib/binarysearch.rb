1304ea31d807442bfb95f3bb49f5d32df1b61cf8
supperbull <knicio123@gmail.com>
2016-12-11 21:52:58 +0100

    Add files via upload
    QUICKSORT binarysearch

---
+++ lib/binarysearch.rb
@@ -0,0 +1,14 @@
+def bsearch(arr, key)
+  min = 0; max = arr.size - 1 
+  while min <= max
+    mid = min + (max-min)/2
+    if key < arr[mid] 
+      max = mid - 1
+    elsif key > arr[mid]
+      min = mid + 1
+    else
+      return mid
+    end
+  end
+  return -1
+end
